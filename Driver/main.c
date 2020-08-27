d6024b8bb219b52716ea12d3746a9f839c79b979
Baris <tuncerb@gmail.com>
2020-08-26 18:20:18 -0700

    initial commit

---
+++ Driver/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//#include "csapp.h"  // will create an error unless linking in mylib/CMakeLists.txt is PUBLIC
+#include "mylib.h"
+// #include "mylib_private.h" -- > will give an error whether linking in mylib/CMakeLists.txt is PUBLIC or PRIVATE
+#include "log.h"     // shouldn't be accessible to mylib
+int main(){
+    printf("Hello csapp!\n");
+    //Sleep(3);   // from "csapp.h"
+    int i = fantastic_sum(3,5);
+    //secret_helper_for_fantastic_sum(int); // from "mylib_private.h"
+    printf("Sum is %d\n",i);
+    log_info("Hello from logging!");
+    log_warn("Are you sure that the value of i should be %d",i);
+    return 0;
+}
---
+++ Mylib/include/mylib.h
@@ -0,0 +1,5 @@
+#ifndef __MYLIB_H__
+#define __MYLIB_H__
+int fantastic_sum(int, int);
+#endif /* __MYLIB_H__ */
+
