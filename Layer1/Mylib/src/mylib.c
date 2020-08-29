4d6345d36b5ad84f7b2b7700be4b833122ed415b
Baris <tuncerb@gmail.com>
2020-08-28 23:13:45 -0700

    initial commit

--- Layer1/Mylib/src/mylib.c
+++ Layer1/Mylib/src/mylib.c
@@ -1,6 +1,7 @@
 #include "csapp.h"
 #include "mylib_private.h"
 #include "mylib.h"
+#include "log.h"
 #include <stdio.h>
 // #include "log.h"  // should not be possible unless mylib links it!
 void secret_helper_for_fantastic_sum(int a){
@@ -10,6 +11,6 @@ void secret_helper_for_fantastic_sum(int a){
 int fantastic_sum(int a , int b){
     printf("fantastic sum is sleeping via secret implementation via Sleep in  csapp\n");
     secret_helper_for_fantastic_sum(3);
-    printf("fantastic sum is returning\n");
+    log_info("Logging from mylib!");
     return a+b;
 }
